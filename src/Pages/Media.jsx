e7e8bff021530bd3b63a664dd2bc45a21f1c33af
Jack Sonoda <jackksono@gmail.com>
2023-07-17 21:10:42 -0700

    Minor fix to contact in media page

--- src/Pages/Media.jsx
+++ src/Pages/Media.jsx
@@ -9,10 +9,13 @@ import Article4 from '../Images/Article4.png'
 import ContactUs from './ContactUs'
 
 const Media = () => {
+  
   const [ contactPopUp, setContactPopUp ] = useState(false)
+
   return (
     <div>
       <div className='grid justify-center w-screen h-full grid-cols-2 p-5 text-black place-items-center '>
+      <ContactUs className='' trigger={contactPopUp} setTrigger={setContactPopUp}></ContactUs>
         <img 
         className='shadow-xl rounded-2xl'
         alt='Headshot'
@@ -28,8 +31,9 @@ const Media = () => {
           Pariatur deleniti omnis nesciunt numquam eius obcaecati, ipsa quibusdam dicta exercitationem quis nulla vero aliquid officia. Perferendis eaque praesentium laudantium exercitationem vero sunt eum nostrum dolorem nemo. Temporibus, error totam?</p>
           <button className='absolute items-center w-[100px] border border-black border-solid hover:bg-black hover:text-white mt-10 rounded-2xl'
             onClick={() => setContactPopUp(true)}>Contact Me
+            
           </button>
-          <ContactUs trigger={contactPopUp} setTrigger={setContactPopUp}></ContactUs>
+         
         </div>
       </div>
       <div className='grid w-screen grid-cols-4 pt-40'>
@@ -37,30 +41,29 @@ const Media = () => {
           target='_blank'
           rel="noopener noreferrer"
           className='flex items-center justify-center w-auto h-auto font-bold text-black '>
-            <p className='flex pb-48 text-xl italic text-black'>Andrew Caroll Shares his top 10 Dishes!</p>
-            <img src={Article1} alt='Article1' className='mt-20 absolute w-[400px] h-[200px] hover:scale-125 cursor-pointer'></img>
+          <p className='flex pb-48 text-xl italic text-black'>Andrew Caroll Shares his top 10 Dishes!</p>
+          <img src={Article1} alt='Article1' className='mt-20 absolute w-[400px] h-[200px] hover:scale-125 cursor-pointer'></img>
         </a>
         <a href='https://observer.com/2016/10/catch-takes-scene-dining-to-new-heights-in-la/'
           target='_blank'
           rel="noopener noreferrer"
           className='flex items-center justify-center w-auto h-auto font-bold text-black'>
-            <p className='flex pb-48 text-xl italic text-black'>Catch Takes Dining Scences to new Heights</p>
-            <img src={Article2} alt='Article2' className='mt-20 absolute w-[400px] h-[200px] hover:scale-125 cursor-pointer'></img>
+          <p className='flex pb-48 text-xl italic text-black'>Catch Takes Dining Scences to new Heights</p>
+          <img src={Article2} alt='Article2' className='mt-20 absolute w-[400px] h-[200px] hover:scale-125 cursor-pointer'></img>
         </a>
         <a href='https://www.reviewjournal.com/entertainment/food/las-vegas-chefs-prepare-vegan-feast-that-was-a-hit-in-new-york-1835770/'
-        target='_blank'
-        rel="noopener noreferrer"
-        className='flex items-center justify-center w-auto h-auto font-bold text-black'>
+          target='_blank'
+          rel="noopener noreferrer"
+          className='flex items-center justify-center w-auto h-auto font-bold text-black'>
           <p className='flex pb-48 text-xl italic text-black'>Chef Prepares Vegan Feast in New York!</p>
           <img src={Article3} alt='Article3' className='mt-20 absolute w-[400px] h-[200px] hover:scale-125 cursor-pointer'></img>
         </a>
         <a href='https://www.visitwesthollywood.com/stories/award-winning-chefs-restaurants-in-weho/'
-        target='_blank'
-        rel="noopener noreferrer"
-        className='flex items-center justify-center w-auto h-auto font-bold text-black'>
+          target='_blank'
+          rel="noopener noreferrer"
+          className='flex items-center justify-center w-auto h-auto font-bold text-black'>
           <p className='flex pb-48 text-xl italic text-black'>Where to Find Award Winning Chefs in West Hollywood</p>
           <img src={Article4} alt='Article4' className='mt-20 absolute w-[400px] h-[200px] hover:scale-125 cursor-pointer'></img>
-        
         </a>
       </div>
     </div>
