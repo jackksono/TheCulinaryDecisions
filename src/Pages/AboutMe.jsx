e513fa0801de3a282d67620d545cb3132cade01c
Jack Sonoda <jackksono@gmail.com>
2023-08-23 19:02:11 -0700

    Added bio for Lev and updated UI/UX

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -46,14 +46,14 @@ const AboutMe = () => {
                   src={Lev}
                 />
               </Fade>
-              <div className='absolute bottom-0 left-0 justify-center duration-700 opacity-0 lg:ml-72 group-hover:opacity-100 '>
-                <p className='pt-5 m-5 lg:m-12 lg:p-5  text-black bg-transparent border lg:h-auto lg:text-xl font-Montserrat lg:leading-8 lg:w-[500px] border-gray-600 text-md rounded-2xl opacity-0 group-hover:opacity-100'>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, dicta temporibus id sapiente aut veniam natus ad neque expedita nulla ea commodi 
-                  animi nisi dignissimos. Quo, aspernatur? Sapiente, cumque velit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid aspernatur dolorem ab, commodi ducimus corrupti harum a quam quidem, dolores id nostrum voluptatibus cumque vel? Reprehenderit nisi distinctio consequatur atque!
-                  Perspiciatis mollitia numquam harum hic praesentium laborum aliquid labore in voluptates? Repellat non voluptatibus illo officia quaerat, laudantium recusandae modi ipsam enim a in quis, beatae reprehenderit? Sunt, enim odit?
-                  Veritatis aliquid assumenda ad aperiam velit error ea similique cumque laborum, unde provident dolor earum ullam at eligendi vel id enim excepturi consectetur exercitationem illo consequuntur nesciunt. Accusamus, quidem ipsam.
-                  Eum aliquam, architecto, aspernatur animi corrupti inventore ab numquam necessitatibus ad voluptas illum a maxime, reiciendis ipsa. Dolorum, nisi sit vitae animi libero, illo in ullam aperiam quo aliquid distinctio.
-                  Pariatur deleniti omnis nesciunt numquam eius obcaecati, ipsa quibusdam dicta exercitationem quis nulla vero aliquid officia. Perferendis eaque praesentium laudantium exercitationem vero sunt eum nostrum dolorem nemo. Temporibus, error totam?
+              <div className='absolute bottom-0 justify-center duration-700 shadow-2xl opacity-0 bg-slate-200 rounded-2xl left-12 lg:ml-72 group-hover:opacity-80'>
+                <p className='pt-5 m-5 lg:m-0 lg:p-5 text-black bg-transparent border lg:h-auto lg:text-xl font-semibold lg:leading-8 lg:w-[500px] text-md rounded-2xl opacity-0 group-hover:opacity-100 font-Montserrat'>
+                A first-generation American, Levan offers a diverse and unique perspective. With over 20 years' experience in luxury resort 
+                operations and leadership, she has collaborated with a variety of hospitality powerhouses across the Las Vegas strip. 
+                Her passion for curating new and innovative concepts led her to spearhead the opening of Resorts World Las Vegas as the Director
+                 of Beverage & Cocktails. She successfully developed strategic programs and creative concepts for over forty food and beverage 
+                 venues. A hospitality professional, her expansive knowledge in new openings and business acumen has dedicated her to Culinary 
+                 Decisions, where she takes immense pride in assisting others in developing effective business strategies.
                 </p>
               </div>
           </div>
@@ -83,31 +83,6 @@ const AboutMe = () => {
           </div>
         </div>
     </div>
-        
-      
-
-      {/* <div ref={zoom3Ref}>
-        <Fade right duration={2000} when={zoom3InView}>
-          <div className='grid justify-center w-screen h-full grid-cols-2 p-5 text-black shadow-sm place-items-center '>
-            <img 
-            className='shadow-xl rounded-full h-[500px] w-auto' 
-            alt='Headshot'
-            src={Drew}>
-            </img>
-            <div className='h-auto pr-20 '>
-              <h1 className='text-4xl font-extrabold'>Chef Andrew Carroll</h1>
-              <p className='relative pt-5 font-sans text-xl text-black '>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, dicta temporibus id sapiente aut veniam natus ad neque expedita nulla ea commodi 
-              animi nisi dignissimos. Quo, aspernatur? Sapiente, cumque velit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid aspernatur dolorem ab, commodi ducimus corrupti harum a quam quidem, dolores id nostrum voluptatibus cumque vel? Reprehenderit nisi distinctio consequatur atque!
-              Perspiciatis mollitia numquam harum hic praesentium laborum aliquid labore in voluptates? Repellat non voluptatibus illo officia quaerat, laudantium recusandae modi ipsam enim a in quis, beatae reprehenderit? Sunt, enim odit?
-              Veritatis aliquid assumenda ad aperiam velit error ea similique cumque laborum, unde provident dolor earum ullam at eligendi vel id enim excepturi consectetur exercitationem illo consequuntur nesciunt. Accusamus, quidem ipsam.
-              Eum aliquam, architecto, aspernatur animi corrupti inventore ab numquam necessitatibus ad voluptas illum a maxime, reiciendis ipsa. Dolorum, nisi sit vitae animi libero, illo in ullam aperiam quo aliquid distinctio.
-              Pariatur deleniti omnis nesciunt numquam eius obcaecati, ipsa quibusdam dicta exercitationem quis nulla vero aliquid officia. Perferendis eaque praesentium laudantium exercitationem vero sunt eum nostrum dolorem nemo. Temporibus, error totam?</p>
-            </div>
-          </div>
-        </Fade>
-      </div> */}
-      
-      {/* <div className='pt-10'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores harum nulla dicta ipsam explicabo commodi possimus. Tempore quaerat facilis similique incidunt dolores? Corrupti ea inventore quis, voluptatem corporis qui ipsum. Laboriosam sunt debitis quasi voluptas id totam maiores esse omnis dolorum, ad porro et, voluptatibus iusto laborum, perferendis est. Doloribus ab temporibus nobis impedit, illum iure praesentium magni deleniti quisquam sint voluptatem soluta pariatur fugiat quam voluptas. Laborum, nobis exercitationem aut molestias ipsa, tempore at molestiae sapiente quas vitae culpa eum architecto facere nulla ab non nesciunt, quaerat delectus obcaecati. Quod inventore cum harum deleniti amet numquam quae dicta doloribus ut sed pariatur culpa non, odio, nam accusantium rem illum ipsam aliquam impedit, quis cupiditate eum? Molestiae voluptatibus, labore maiores et nisi voluptatum cumque eligendi. Accusantium maxime esse libero ex labore? Placeat laudantium labore totam voluptate debitis. Doloremque architecto illum quas alias vitae aperiam iste provident molestiae ullam unde, quia nostrum laudantium a nihil sunt voluptatibus ut, officiis commodi iure quo. Optio placeat consequatur non praesentium dolorem ad corrupti, asperiores fugiat esse exercitationem voluptas recusandae voluptatibus dolor eaque rerum? Voluptate sed magni voluptatum quos ducimus tempora deleniti magnam alias eligendi, officiis iste consequatur odit tenetur eum dignissimos? Corporis eveniet commodi veritatis ipsa eos. Tenetur rerum fuga minus magni doloremque. Necessitatibus doloribus dolorem pariatur, laudantium dolore cum nihil exercitationem error facilis modi eius fugit veniam nemo? Doloribus recusandae inventore voluptatibus, cupiditate obcaecati eveniet quo aliquid unde velit exercitationem suscipit atque libero quis a magni cum voluptatum repudiandae vero ipsum quam quia perferendis, maiores rerum? Suscipit possimus omnis fugiat aliquam alias quis assumenda, libero enim aliquid perspiciatis. Rerum tempore dicta nemo! Itaque quasi minima cupiditate, provident excepturi hic sit dolorem. Exercitationem odit aspernatur laudantium, amet dolorem molestiae et expedita reprehenderit debitis dicta? Magnam, architecto exercitationem nobis illo molestiae ipsam quo, neque minus recusandae aut ipsum deserunt? Porro repellat eveniet temporibus dolorem rerum labore accusantium omnis, beatae, corporis recusandae voluptatum magni commodi facilis explicabo maxime fugiat id ut odio fugit sunt natus praesentium, mollitia minima ratione. Soluta quis quas consequatur modi neque expedita, corporis et doloremque sunt, vero vitae inventore perspiciatis sapiente quos? Error commodi non fugiat itaque aliquam nisi culpa temporibus vitae distinctio amet quo nemo rerum, earum tempore! Aspernatur libero doloremque, maiores modi nostrum et? Corrupti rem libero corporis eius placeat laboriosam sed eaque veritatis quae recusandae asperiores minima, a doloremque, voluptates incidunt! Quam minima facilis soluta maxime provident, consequatur eos. In ad omnis a perspiciatis repudiandae minus recusandae porro maiores earum, sint quod, saepe dignissimos similique quia nisi dolores, beatae optio eaque provident sed modi unde. Porro, cupiditate ex sunt sit magni ipsa voluptatum, inventore similique veniam debitis quo harum possimus fugiat impedit nostrum animi, a vitae soluta reiciendis tempore eveniet unde saepe natus atque. Obcaecati, ab facere! Necessitatibus veritatis itaque suscipit optio, soluta quo nesciunt, vero velit aliquid adipisci, veniam ab magnam. Impedit magnam laudantium consequatur voluptatem tempora maxime repudiandae rerum libero laboriosam fugit minus dolores nihil ut, quo et cumque. Fuga ducimus vel culpa et sit sint sed.</div> */}
     </div> //Maybe compile media and about me
   )
 }
