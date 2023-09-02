8d8a20c52bfc4b2f0409fb73b7bf137e82b6613e
Jack Sonoda <jackksono@gmail.com>
2023-09-01 17:07:44 -0700

    Added images for services

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import Carousel from 'react-elastic-carousel';
 
 import '../index.css'
-import service1 from '../Images/Slideshow/OnePOne.jpg'
+import BrandMarketing from '../Images/Services/BrandMarketing.jpg'
+import EventPlanner from '../Images/Services/EventPlanner.jpg'
+import FoodandBeverage from '../Images/Services/FoodandBeverage.jpg'
+import HospitalityConsulting from '../Images/Services/HospitalityConsulting.jpg'
+import NightlifeConsulting from '../Images/Services/NightlifeConsulting.jpg'
+import OpenVenueProtocol from '../Images/Services/OpenVenueProtocol.jpg'
+import Sourcing from '../Images/Services/Sourcing.jpg'
+import WebDevelopment from '../Images/Services/WebDevelopment.jpg'
+import MerchandiseProduction from '../Images/Services/MerchandiseProduction.jpg'
 
 
 const breakPoints = [
   {width: 1, itemsToShow: 1},
-  {width: 550, itemsToShow: 2},
+  {width: 550, itemsToShow: 3},
 ]
 
 const AboutUs = () => {
@@ -21,7 +29,7 @@ const AboutUs = () => {
         <div className='mt-20 lg:mt-80'>
             <Carousel breakPoints={breakPoints} className='px-2'>
               <div className='flex flex-col items-center justify-center place-content-center mr-[20px]'>
-                <img className='lg:w-[600px] lg:h-[400px]' alt='service1' src={service1} loading='lazy'></img>
+                <img className='' alt='service1' src={BrandMarketing} loading='lazy'></img>
                 <div className='flex flex-col items-start w-full'>
                   <h1 className='flex pt-3 font-extrabold text-black lg:px-5 font-Montserrat lg:text-3xl'>BRAND MARKETING</h1>
                   <a href='youtube.com' target="_blank" rel="noopener noreferrer" className='text-blue-300 lg:px-10 lg:pt-5 hover:text-blue-500'>Learn more...</a>
@@ -29,7 +37,7 @@ const AboutUs = () => {
               </div>
 
               <div className='flex flex-col items-center justify-center place-content-center mr-[20px]'>
-                <img className='lg:w-[600px] lg:h-[400px]' alt='service1' src={service1} loading='lazy'></img>
+                <img className='' alt='service1' src={MerchandiseProduction} loading='lazy'></img>
                 <div className='flex flex-col items-start w-full'>
                   <h1 className='flex pt-3 font-extrabold text-black lg:px-5 font-Montserrat lg:text-3xl'>CUSTOM MERCHANDISE PRODUCTION</h1>
                  
@@ -38,7 +46,7 @@ const AboutUs = () => {
               </div>
 
               <div className='flex flex-col items-center justify-center place-content-center mr-[20px]'>
-                <img className='lg:w-[600px] lg:h-[400px]' alt='service1' src={service1} loading='lazy'></img>
+                <img className='' alt='service1' src={EventPlanner} loading='lazy'></img>
                 <div className='flex flex-col items-start w-full'>
                   <h1 className='flex pt-3 font-extrabold text-black lg:px-5 font-Montserrat lg:text-3xl'>EVENT PLANNING</h1>
                   <a href='youtube.com' target="_blank" rel="noopener noreferrer" className='text-blue-300 lg:px-10 lg:pt-5 hover:text-blue-500'>Learn more...</a>
@@ -46,7 +54,7 @@ const AboutUs = () => {
               </div>
 
               <div className='flex flex-col items-center justify-center place-content-center mr-[20px]'>
-                <img className='lg:w-[600px] lg:h-[400px]' alt='service1' src={service1} loading='lazy'></img>
+                <img className='h-[657.5px]' alt='service1' src={FoodandBeverage} loading='lazy'></img>
                 <div className='flex flex-col items-start w-full'>
                   <h1 className='flex pt-3 font-extrabold text-black lg:px-5 font-Montserrat lg:text-3xl'>FOOD & BEVERAGE CONSULTING</h1>
                   <a href='youtube.com' target="_blank" rel="noopener noreferrer" className='text-blue-300 lg:px-10 lg:pt-5 hover:text-blue-500'>Learn more...</a>
@@ -54,7 +62,7 @@ const AboutUs = () => {
               </div>
 
               <div className='flex flex-col items-center justify-center place-content-center mr-[20px]'>
-                <img className='lg:w-[600px] lg:h-[400px]' alt='service1' src={service1} loading='lazy'></img>
+                <img className='' alt='service1' src={HospitalityConsulting} loading='lazy'></img>
                 <div className='flex flex-col items-start w-full'>
                   <h1 className='flex pt-3 font-extrabold text-black lg:px-5 font-Montserrat lg:text-3xl'>HOSPITALITY CONSULTING</h1>
                   <a href='youtube.com' target="_blank" rel="noopener noreferrer" className='text-blue-300 lg:px-10 lg:pt-5 hover:text-blue-500'>Learn more...</a>
@@ -64,7 +72,7 @@ const AboutUs = () => {
               
 
               <div className='flex flex-col items-center justify-center place-content-center mr-[20px]'>
-                <img className='lg:w-[600px] lg:h-[400px]' alt='service1' src={service1} loading='lazy'></img>
+                <img className='' alt='service1' src={NightlifeConsulting} loading='lazy'></img>
                 <div className='flex flex-col items-start w-full'>
                   <h1 className='flex pt-3 font-extrabold text-black lg:px-5 font-Montserrat lg:text-3xl'>NIGHTLIFE CONSULTING</h1>
                   <a href='youtube.com' target="_blank" rel="noopener noreferrer" className='text-blue-300 lg:px-10 lg:pt-5 hover:text-blue-500'>Learn more...</a>
@@ -72,7 +80,7 @@ const AboutUs = () => {
               </div>
 
               <div className='flex flex-col items-center justify-center place-content-center mr-[20px]'>
-                <img className='lg:w-[600px] lg:h-[400px]' alt='service1' src={service1} loading='lazy'></img>
+                <img className='' alt='service1' src={OpenVenueProtocol} loading='lazy'></img>
                 <div className='flex flex-col items-start w-full'>
                   <h1 className='flex pt-3 font-extrabold text-black lg:px-5 font-Montserrat lg:text-3xl'>OPEN VENUE PROTOCOL</h1>
                   
@@ -81,7 +89,7 @@ const AboutUs = () => {
               </div>
 
               <div className='flex flex-col items-center justify-center place-content-center mr-[20px]'>
-                <img className='lg:w-[600px] lg:h-[400px]' alt='service1' src={service1} loading='lazy'></img>
+                <img className='' alt='service1' src={Sourcing} loading='lazy'></img>
                 <div className='flex flex-col items-start w-full'>
                   <h1 className='flex pt-3 font-extrabold text-black lg:px-5 font-Montserrat lg:text-3xl'>SOURCING</h1>
                   <a href='https://youtube.com' target="_blank" rel="noopener noreferrer" className='text-blue-300 lg:px-10 lg:pt-5 hover:text-blue-500'>Learn more...</a>
@@ -89,7 +97,7 @@ const AboutUs = () => {
               </div>
 
               <div className='flex flex-col items-center justify-center place-content-center mr-[20px]'>
-                <img className='lg:w-[600px] lg:h-[400px]' alt='service1' src={service1} loading='lazy'></img>
+                <img className='' alt='service1' src={WebDevelopment} loading='lazy'></img>
                 <div className='flex flex-col items-start w-full'>
                   <h1 className='flex pt-3 font-extrabold text-black lg:px-5 font-Montserrat lg:text-3xl'>WEB DEVELOPMENT</h1>
                   <a href='https://youtube.com' target="_blank" rel="noopener noreferrer" className='text-blue-300 lg:px-10 lg:pt-5 hover:text-blue-500'>Learn more...</a>
